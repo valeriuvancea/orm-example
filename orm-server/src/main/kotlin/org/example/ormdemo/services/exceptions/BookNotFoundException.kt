55bbe41919d8d0c4f3329ac07da0dacd93f846fa
Nicolas Vancea <valeriuvancea@gmail.com>
2023-12-07 21:41:47 +0100

    Add open api

--- orm-server/src/main/kotlin/org/example/ormdemo/services/exceptions/BookNotFoundException.kt
+++ orm-server/src/main/kotlin/org/example/ormdemo/services/exceptions/BookNotFoundException.kt
@@ -1,3 +1,7 @@
 package org.example.ormdemo.services.exceptions
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class BookNotFoundException(id: Long) : RuntimeException("Book with id `$id` not found!")
--- orm-server/src/main/kotlin/org/example/ormdemo/services/exceptions/EntityNotFoundException.kt
+++ orm-server/src/main/kotlin/org/example/ormdemo/services/exceptions/EntityNotFoundException.kt
@@ -1,6 +1,9 @@
 package org.example.ormdemo.services.exceptions
 
 import org.example.ormdemo.entities.BaseEntity
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class EntityNotFoundException(id: Long, entityClass: Class<out BaseEntity?>) :
     RuntimeException("Entity `${entityClass.simpleName}` with id `$id` not found!")
